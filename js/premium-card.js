b3a19b97e6d9f3a1038b5be787c6629318b3d277
Илья <67915032+Ilya-Saldeckiy@users.noreply.github.com>
2021-01-19 17:45:39 +0300

    Add files via upload

---
+++ js/premium-card.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#btn').addEventListener('click', function () {
+        document.querySelector('.premium-btn-position').classList.toggle('disable-btn') && document.querySelector('.premium-btn-position').classList.remove('active-prem')
+    })
+    document.querySelector('.premium-btn').addEventListener('click', function () {
+        document.querySelector('.decoration-card').classList.toggle('activ-btn') && document.querySelector('.decoration-card').classList.remove('disable-card')
+    })
+    document.querySelector('#continue').addEventListener('click', function () {
+        document.querySelector('.second-step-premium').classList.toggle('activ-btn') && document.querySelector('.second-step-premium').classList.remove('disable-card')
+    })
+
+    document.querySelector('#continue').addEventListener('click', function () {
+        document.querySelector('.first-step-premium').classList.toggle('disable-card') && document.querySelector('.first-step-premium').classList.remove('active-prem')
+    })
+
+    document.querySelector('#btnBack').addEventListener('click', function () {
+        document.querySelector('.second-step-premium').classList.toggle('disable-card') && document.querySelector('.second-step-premium').classList.remove('activ-btn')
+    })
+
+    document.querySelector('#btnBack').addEventListener('click', function () {
+        document.querySelector('.first-step-premium').classList.toggle('active-prem') && document.querySelector('.first-step-premium').classList.remove('disable-card')
+    })
+
+    document.querySelector('#addBtn').addEventListener('click', function () {
+        document.querySelector('.add-person-block').classList.toggle('activ-btn') && document.querySelector('.add-person-block').classList.remove('disable-card')
+    })
+
+    document.querySelector('#addBtn').addEventListener('click', function () {
+        document.querySelector('#addBtn').classList.toggle('disable-btn') && document.querySelector('#addBtn').classList.remove('disable-card')
+    })
+
+    document.querySelector('#btnContinui').addEventListener('click', function () {
+        document.querySelector('.three-step').classList.toggle('activ-btn') && document.querySelector('.three-step').classList.remove('disable-card')
+    })
+
+    document.querySelector('#btnContinui').addEventListener('click', function () {
+        document.querySelector('.second-step-premium').classList.toggle('disable-card') && document.querySelector('.second-step-premium').classList.remove('active-prem')
+    })
+
+
+})
