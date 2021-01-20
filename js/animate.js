7c47ebc5f4d34b219041a7cc1ce35a9d7486fdf7
Илья <67915032+Ilya-Saldeckiy@users.noreply.github.com>
2021-01-20 15:17:09 +0300

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".anchor").on("click","a", function (event) {
+    $(".menu-list-item").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
