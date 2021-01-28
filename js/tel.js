306878f18d535492c112fc015aaca9124ae26446
Илья <67915032+Ilya-Saldeckiy@users.noreply.github.com>
2021-01-28 14:13:32 +0300

    Add files via upload

--- js/tel.js
+++ js/tel.js
@@ -1,4 +1,24 @@
 $(function() {
   $(".phone").mask("+7(999) 999-99-99");
-  $(".input-date").mask("99.99.9999")
+ });
+
+$(function(){
+  $('.input-date').daterangepicker({
+   singleDatePicker: true,
+   autoUpdateInput: false,
+   autoApply: true,
+   locale: {
+    cancelLabel: 'Clear',
+    format: 'DD.MM.YYYY',
+    autoclose: true,
+   }
+  });
+  
+  $('.input-date').on('apply.daterangepicker', function(ev, picker) {
+      $(this).val(picker.startDate.format('DD.MM.YYYY'));
+  });
+
+  $('.input-date').on('cancel.daterangepicker', function(ev, picker) {
+      $(this).val('');
+  });
  });
